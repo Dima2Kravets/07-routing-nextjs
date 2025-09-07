f879a780fd55537421bb4917f3031a070e4cd5b6
Dima_Kravets <dima2.kravets@gmail.com>
2025-09-07 21:34:53 +0300

    Implement sidebar and layout components with styles for notes filtering

--- app/notes/filter/layout.tsx
+++ app/notes/filter/layout.tsx
@@ -1,5 +1,5 @@
 import { Suspense } from "react";
-import css from "@/app/notes/LayoutNotes.module.css";
+import css from "@/app/notes/filter/LayoutNotes.module.css";
 export default function LayoutNotes({
   children,
   sidebar,
